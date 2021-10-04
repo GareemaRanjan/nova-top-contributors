004747139fa1f3662a11a44207710ac0a7beb8ac
Gareema <garranja@cisco.com>
2021-10-04 11:40:22 +0530

    Fixing openstack-gerrit commits

--- get_top_authors.py
+++ get_top_authors.py
@@ -9,20 +9,30 @@ from github import Github
 past_date = datetime.now() - relativedelta(months=6)
 g = Github(login_or_token=sys.argv[1])
 repo = g.get_repo(full_name_or_id='openstack/nova')
-
 print("Starting to collect commits...")
 # Initialise auth_dict to contain list of authors and number of commits.
-auth_dict = dict(No_comm=0)
+auth_dict = dict()
+print(f"total {repo.get_commits(since=past_date).totalCount}")
 for commit in repo.get_commits(since=past_date):
-    if not commit.committer:
-        auth_dict['No_comm'] +=1
-    else:
-        if auth_dict.get(commit.committer.login):
-            auth_dict[commit.committer.login] +=1
-        else:
-            auth_dict[commit.committer.login] = 1
+    commit_author = None
+    if commit.committer:
+        if commit.committer.login == 'openstack-gerrit':
+            parentlist = commit.parents
+            for parent in parentlist:
+                if parent.committer and parent.committer.login != 'openstack-gerrit':
+                    commit_author = parent.committer.login # what to be used here - author or committer?
+        elif commit.committer.login:
+            commit_author = commit.committer.login
+        try:
+            g.get_user(commit_author)
+            if auth_dict.get(commit_author):
+                auth_dict[commit_author] +=1
+            else:
+                auth_dict[commit_author] = 1
+        except Exception as e:
+            print(f"user {commit_author} , {commit.html_url} not valid")
 
-
+print(auth_dict)
 top_12=sorted(auth_dict.items(), key=lambda x:x[1], reverse=True)[:12]
 df = pd.DataFrame(top_12, columns=['Contributor','Number of commits'])
 print(df)
